0f00e5dfcd65be04a304b4c0e995eadfa1dcac56
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-04 09:10:26 +0700

    feat: add repo for get products

--- internal/repository/product.repository.go
+++ internal/repository/product.repository.go
@@ -1,1 +1,25 @@
 package repository
+
+import "github.com/virgilIw/koda-b6-backend2/internal/model"
+
+var productData = []model.ProductModel{
+	{
+		Id:          1,
+		ProductName: "Kopi tubruk",
+		Rating:      4.8,
+	},
+}
+
+type ProductRepository struct {
+	db []model.ProductModel
+}
+
+func NewProductRepository(db *[]model.ProductModel) *ProductRepository {
+	return &ProductRepository{
+		db: productData,
+	}
+}
+
+func (p *ProductRepository) GetProducts() []model.ProductModel {
+	return p.db
+}
