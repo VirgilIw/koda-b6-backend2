775e01c86f2280cfcdd26b966504b4039de4dd2c
VirgilIw <imanuelambar11@gmail.com>
2026-03-05 10:27:32 +0700

    Update product.repository.go

--- internal/repository/product.repository.go
+++ internal/repository/product.repository.go
@@ -14,12 +14,59 @@ type ProductRepository struct {
 	db []model.ProductModel
 }
 
-func NewProductRepository(db *[]model.ProductModel) *ProductRepository {
+func NewProductRepository() *ProductRepository {
 	return &ProductRepository{
 		db: productData,
 	}
 }
 
+// Get all products
 func (p *ProductRepository) GetProducts() []model.ProductModel {
 	return p.db
+}
+
+// Get product by ID
+func (p *ProductRepository) GetProductById(id int) (model.ProductModel, bool) {
+	for _, product := range p.db {
+		if product.Id == id {
+			return product, true
+		}
+	}
+	return model.ProductModel{}, false
+}
+
+// Create product
+func (p *ProductRepository) CreateProduct(product model.ProductModel) model.ProductModel {
+	product.Id = len(p.db) + 1
+	p.db = append(p.db, product)
+	return product
+}
+
+// Update product
+func (p *ProductRepository) UpdateProduct(id int, updated model.ProductModel) (model.ProductModel, bool) {
+	for i, product := range p.db {
+		if product.Id == id {
+			p.db[i].ProductName = updated.ProductName
+			p.db[i].Rating = updated.Rating
+			return p.db[i], true
+		}
+	}
+	return model.ProductModel{}, false
+}
+
+// Delete product
+func (p *ProductRepository) DeleteProduct(id int) model.ProductModel {
+	var deleted model.ProductModel
+	newData := []model.ProductModel{}
+
+	for _, product := range p.db {
+		if product.Id == id {
+			deleted = product
+			continue
+		}
+		newData = append(newData, product)
+	}
+
+	p.db = newData
+	return deleted
 }
