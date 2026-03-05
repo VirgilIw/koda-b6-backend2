6f2eb20db1fd0ed60660b140a60fda42698b6def
VirgilIw <imanuelambar11@gmail.com>
2026-03-05 10:29:42 +0700

    Update product.service.go

--- internal/service/product.service.go
+++ internal/service/product.service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/virgilIw/koda-b6-backend2/internal/dto"
+	"github.com/virgilIw/koda-b6-backend2/internal/model"
 	"github.com/virgilIw/koda-b6-backend2/internal/repository"
 )
 
@@ -15,6 +16,7 @@ func NewProductService(repository *repository.ProductRepository) *ProductService
 	}
 }
 
+// Get all products
 func (p *ProductService) GetProducts() []dto.ProductsDto {
 	data := p.repo.GetProducts()
 
@@ -27,5 +29,74 @@ func (p *ProductService) GetProducts() []dto.ProductsDto {
 			Rating:      v.Rating,
 		})
 	}
+
 	return result
+}
+
+// Get product by ID
+func (p *ProductService) GetProductById(id int) dto.ProductsDto {
+	data, found := p.repo.GetProductById(id)
+
+	if !found {
+		return dto.ProductsDto{}
+	}
+
+	return dto.ProductsDto{
+		Id:          data.Id,
+		ProductName: data.ProductName,
+		Rating:      data.Rating,
+	}
+}
+
+// Create product
+func (p *ProductService) CreateProduct(req dto.ProductsDto) dto.ProductsDto {
+
+	product := model.ProductModel{
+		ProductName: req.ProductName,
+		Rating:      req.Rating,
+	}
+
+	data := p.repo.CreateProduct(product)
+
+	return dto.ProductsDto{
+		Id:          data.Id,
+		ProductName: data.ProductName,
+		Rating:      data.Rating,
+	}
+}
+
+// Update product
+func (p *ProductService) UpdateProduct(id int, req dto.ProductsDto) dto.ProductsDto {
+
+	product := model.ProductModel{
+		ProductName: req.ProductName,
+		Rating:      req.Rating,
+	}
+
+	data, found := p.repo.UpdateProduct(id, product)
+
+	if !found {
+		return dto.ProductsDto{}
+	}
+
+	return dto.ProductsDto{
+		Id:          data.Id,
+		ProductName: data.ProductName,
+		Rating:      data.Rating,
+	}
+}
+
+// Delete product
+func (p *ProductService) DeleteProduct(id int) dto.ProductsDto {
+	data := p.repo.DeleteProduct(id)
+
+	if data.Id == 0 {
+		return dto.ProductsDto{}
+	}
+
+	return dto.ProductsDto{
+		Id:          data.Id,
+		ProductName: data.ProductName,
+		Rating:      data.Rating,
+	}
 }
